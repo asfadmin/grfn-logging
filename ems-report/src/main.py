1d7837915e86e5d9d494c78160e9f2536534b468
Andrew Johnston <asjohnston@alaska.edu>
2018-10-05 00:06:57 +0000

    first kind-of working draft of ems-report lambda

--- ems-report/src/main.py
+++ ems-report/src/main.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from os import getenv
 from logging import getLogger
 from datetime import datetime
@@ -32,22 +33,33 @@ def get_elasticsearch_connection(host):
     return es
 
 
-def update_elasticsearch(records, config):
-    es = get_elasticsearch_connection(config['host'])
-    if not es.indices.exists(config['index']):
-        es.indices.create(config['index'], body=index_body)
-    for record in records:
-        es.index(index=config['index'], id=record['id'], doc_type='log', body=record['data'])
-
-
-def generate_ems_report(config):
-    start = datetime.datetime(2018, 10, 4, 0, 0, 0)
-    end = datetime.datetime(2018, 10, 4, 23, 59, 0)
-    records = get_records_by_date(start, end, config['host'])
-    report_content = generate_report_content(records)
-    upload_report_content(report_content, config['output_bucket'])
+def get_category(file_name):
+    if file_name.endswith('.png'):
+        category = 'SENTINEL-1_INSAR_FULL_RES_WRAPPED_INTERFEROGRAM_AND_DEM_BROWSE (BETA)'
+    elif file_name.endswith('.unw_geo.zip'):
+        category = 'SENTINEL-1_INSAR_UNWRAPPED_INTERFEROGRAM_AND_COHERENCE_MAP (BETA)'
+    elif file_name.endswith('.full_res.zip'):
+        category = 'SENTINEL-1_INSAR_FULL_RES_WRAPPED_INTERFEROGRAM_AND_DEM (BETA)'
+    else:
+        category = 'SENTINEL-1_INSAR_ALL_INTERFEROMETRIC_PRODUCTS (BETA)'
+    return category
 
 
 def lambda_handler(event, context):
     config = setup()
-    #generate_ems_report(config)
+    es = get_elasticsearch_connection(config['host'])
+    results = es.search(
+        index=config['index'],
+        doc_type='log',
+        _source_include=['request_time', 'ip_address', 'user_id', 'http_status', 'file_name', 'bytes_sent'],
+        size=10,
+        q='user_id:asjohnston',
+    )
+    records = [result['_source'] for result in results['hits']['hits']]
+    with open('/tmp/ems.csv', 'w') as f:
+        for r in records:
+            r['category'] = get_category(r['file_name'])
+            r['formatted_time'] = datetime.strftime(datetime.strptime(r['request_time'], '%Y-%m-%dT%H:%M:%S+00:00'), '%d/%b/%Y:%H:%M:%S')
+            f.write('[{formatted_time}]|&|{category}|&|{ip_address}|&|{user_id}|&|{bytes_sent}|&|{http_status}\n'.format(**r))
+    s3 = boto3.client('s3')
+    s3.upload_file('/tmp/ems.csv', 'asjohnston-dev', 'ems.csv')
