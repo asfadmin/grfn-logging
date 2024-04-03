58ea750ef63bb5d8e81a59c472fcabc7977d7171
Jake Herrmann <jtherrmann@alaska.edu>
2024-04-03 12:38:15 -0800

    Implement test_get_category for current behavior

--- tests/test_ems_report.py
+++ tests/test_ems_report.py
@@ -2,5 +2,6 @@ import ems_report
 
 
 def test_get_category():
-    # TODO
-    assert False
+    assert ems_report.get_category('foo.png') == 'SENTINEL-1_INTERFEROGRAMS_BROWSE'
+    assert ems_report.get_category('foo.unw_geo.zip') == 'SENTINEL-1_INSAR_UNWRAPPED_INTERFEROGRAM_AND_COHERENCE_MAP'
+    assert ems_report.get_category('foo') == 'SENTINEL-1_INTERFEROGRAMS'
